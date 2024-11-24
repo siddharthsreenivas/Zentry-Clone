eb5b2e9b8b9d64cbe591c18cea51f049bc1b4a6e
Siddharth <siddhusreeni@gmail.com>
2024-11-25 01:58:30 +0530

    contact & footer added

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -74,7 +74,7 @@ const Features = () => {
             src="videos/feature-1.mp4"
             title={
               <>
-                radi<b>n</b>t
+                radia<b>n</b>t
               </>
             }
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
