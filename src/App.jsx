f924fdbf269b820b2abfd866fc74c58762008453
Siddharth <siddhusreeni@gmail.com>
2024-11-24 17:58:13 +0530

    about section and animated title added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Hero from './components/Hero'
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className='z-0 min-h-screen bg-blue-500'></section>
+      <About />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
